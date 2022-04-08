c33704a160ef102de8402f029ff06e9d8abbf678
Leo <leonidas.j.mbm@gmail.com>
2022-04-08 15:25:22 +0200

    exo 3.2

--- 3.1.js
+++ 3.1.js
@@ -2,7 +2,7 @@ let arr = [1, 2, 3, 4, 5]
 let arr2 = [100, 101, 102]
 let total = 0;
 
-for (let i in arr) {
+for (let i in arr / arr.length;) {
   total += arr[i];
   
 
---
+++ 3.2.js
@@ -0,0 +1,23 @@
+let arr = [1, 2, 3, 4, 5] 
+let arr2 = [100, 101, 102]
+let total = 0;
+let total2 = 0;
+
+for (let i in arr) {
+  total += arr[i];
+  avg = total / arr.length;
+  
+
+}
+
+console.log(avg)
+
+
+for (let i in arr2) {
+    total2 += arr2[i];
+    avg2 = total2 / arr2.length;
+    
+  
+  }
+  
+  console.log(avg2)
