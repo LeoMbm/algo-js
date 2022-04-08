dd3dfe6527dcb81eba0bdd50a9e27113379c07f6
Leo <leonidas.j.mbm@gmail.com>
2022-04-08 09:18:00 +0200

    exo 2.4

--- 1.1.js
+++ 1.1.js
@@ -1,5 +1,9 @@
-for (let i = 1; i < 100; i++) {
+for (let i = 1; i <= 100; i += 1) {
   if (i % 2 == 0) {
-    console.log("Number is even:" + i)
+    console.log("Number is even:" + i / 2  )
   } 
+
+  else {
+    console.log("Number is odd:" + i * 3  )
+  }
 }
