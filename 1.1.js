fee798c6554bfe0b068fc3697748126f699a1d79
Leo <leonidas.j.mbm@gmail.com>
2022-04-08 10:45:17 +0200

    exo 2.6

--- 1.1.js
+++ 1.1.js
@@ -1,7 +1,30 @@
-var x =Number(prompt("What is your favorite number ?"))
+let day = prompt('Type a number between 1 and 7');
 
-while (x !== 42){
-  alert("Again ?")
-  x=Number (prompt("What is your favorite number"))
+while ((day < 1)|| (day > 7)) {
+  day = prompt('Enter a number between One and Seven');
+}
 
-}                        
+  switch(day) {
+
+    case "1" : 
+      alert('Lundi');
+      break;
+    case "2" : 
+      alert('Mardi');
+      break;
+    case "3" : 
+      alert('Mercredi');
+      break;
+    case "4" : 
+      alert('Jeudi');
+      break;
+    case "5" : 
+      alert('Vendredi');
+      break;
+    case "6" : 
+      alert('Samedi');
+      break;
+    case "7" : 
+      alert('Dimanche');
+      break;
+  }
