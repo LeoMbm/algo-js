f39272f11b378ba62cf1a22041374d7b0a0037d9
Leo <leonidas.j.mbm@gmail.com>
2022-04-08 10:49:57 +0200

    exo 2.7

--- 1.1.js
+++ 1.1.js
@@ -1,30 +1,14 @@
-let day = prompt('Type a number between 1 and 7');
+let n = Number(prompt("Please enter a number"));
+let final = 0;
+let ask = 0;
 
-while ((day < 1)|| (day > 7)) {
-  day = prompt('Enter a number between One and Seven');
+
+while (n != 0) {
+
+    ask= Number(prompt("Please enter a number"));
+    final += ask;
+    n -=1
 }
 
-  switch(day) {
 
-    case "1" : 
-      alert('Lundi');
-      break;
-    case "2" : 
-      alert('Mardi');
-      break;
-    case "3" : 
-      alert('Mercredi');
-      break;
-    case "4" : 
-      alert('Jeudi');
-      break;
-    case "5" : 
-      alert('Vendredi');
-      break;
-    case "6" : 
-      alert('Samedi');
-      break;
-    case "7" : 
-      alert('Dimanche');
-      break;
-  }
+alert("The result is " + final)
